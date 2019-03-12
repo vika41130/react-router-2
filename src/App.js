945e6f078799e87bde815368c502bbe5c1779970
VN\dung.phan <dung.phan@vn.defide.co.jp>
2019-03-12 11:03:12 +0700

    #router-2-basic - first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, NavLink, Redirect} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+
+const User = ({match}) => {
+  return(
+    <h1> Welcome User - { match.params.username }</h1>
+  );
+}
 
 class App extends Component {
+  state = {
+    loggedin: false
+  }
+
+  loginHandle = () => {
+    this.setState({loggedin: true});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+          <div className="App">
+          <ul>
+            <li>
+              <NavLink to="/" exact activeStyle={{color:'green'}}>Home</NavLink>
+              </li>
+            <li>
+              <NavLink to="/about/" exact activeStyle={{color:'green'}}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/user/" exact activeStyle={{color:'green'}}>User</NavLink>
+            </li>
+
+            {/* <li><Link to="/">Home</Link></li>
+            <li><Link to="/about/">About</Link></li> */}
+          </ul>
+
+          {/* <input type="button" value="log in" onClick={this.loginHandle.bind(this)}/> */}
+
+            <Route exact path="/" strict render={
+              () => {
+                return (<h1>Home</h1>)
+              }
+            }/>
+            <Route exact path="/about/" strict render={
+              () => {
+                return (<h1>About</h1>)
+              }
+            }/>
+            <Route exact path="/user/" strict component={User}/>
+            <Route exact path="/user/:username" strict component={User}/>
+            {/* <Route exact path="/user/:username" strict render={()=> (
+              this.state.loggedin ? ( <User />) : (<Redirect to="/" />)
+            )}/> */}
+          </div>
+        </Router>
     );
   }
 }
